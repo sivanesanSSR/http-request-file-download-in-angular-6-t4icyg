f9509174e1e1ec32bde664caa0fd119e659e2e97
sivanesanSSR <66293642+sivanesanSSR@users.noreply.github.com>
2023-05-18 20:18:27 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# http-request-file-download-in-angular-6-t4icyg
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/call-http-request-in-angular-6-t4icyg)
---
+++ src/app/app.component.ts
@@ -0,0 +1,48 @@
+import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  url = 'https://api.nylas.com/files/bbqmiacejeeq1yydy2p0wq65u/download';
+  fileValid:boolean = true
+  headers: HttpHeaders;
+  constructor(private http: HttpClient) { 
+    this.headers = new HttpHeaders();
+  
+    this.headers = this.headers.append('Accept', 'application/json');
+    this.headers = this.headers.append(
+      'Authorization',
+      'Bearer 5wGeWvlwHc8DFQE1atJGgrPG9vW7dJ'
+    );
+  }
+
+  
+  download()
+  {
+    this.http.get(`https://api.nylas.com/files/bbqmiacejeeq1yydy2p0wq65u/download`,{
+      responseType: 'arraybuffer',headers:this.headers} 
+     ).subscribe(response => this.downLoadFile(response, "application/ms-excel"));
+  }
+   
+  
+  
+  
+  downLoadFile(data: any, type: string) {
+  let blob = new Blob([data], { type: type});
+  let url = window.URL.createObjectURL(blob);
+  let pwa = window.open(url);
+  if (!pwa || pwa.closed || typeof pwa.closed == 'undefined') {
+      alert( 'Please disable your Pop-up blocker and try again.');
+  }
+  }
+ 
+
+
+}
+interface Kafein {
+  name:string;
+  address:string;
+}
